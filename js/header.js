a382daa046c510326024ff5837f3e64d6b01cf60
dupperin <rkawk2239@naver.com>
2023-10-03 03:55:05 +0900

    header nav 작업

--- js/header.js
+++ js/header.js
@@ -1,1 +1,34 @@
 import { qs, qsAll } from "./function.js";
+
+qsAll('li.hover-menu').forEach(li => {
+  li.addEventListener('mouseenter', e => {
+    li.classList.add('active');
+    let subMenuHeight = li.children[1].offsetHeight;
+    const headerNavHeight = qs('ul.header-main').offsetHeight;
+    qs('div.sub-background').style.height = subMenuHeight + headerNavHeight + 'px';
+  });
+
+  li.addEventListener('mouseleave', e => {
+    li.classList.remove('active');
+    qs('div.sub-background').style.height = '0px';
+  });
+});
+
+
+qsAll('li.click-menu').forEach(li => {
+  li.children[0].addEventListener('click', e => {
+    li.classList.toggle('active');
+    let subMenuHeight = li.children[1].offsetHeight;
+    const headerNavHeight = qs('ul.header-main').offsetHeight;
+    if(qs('div.sub-background').style.height === '' || qs('div.sub-background').style.height === '0px'){
+      qs('div.sub-background').style.height = subMenuHeight + headerNavHeight + 'px';
+    }else {
+      qs('div.sub-background').style.height = '0px'
+    }
+  });
+
+  li.addEventListener('mouseleave', e => {
+    li.classList.remove('active');
+    qs('div.sub-background').style.height = '0px';
+  });
+});
